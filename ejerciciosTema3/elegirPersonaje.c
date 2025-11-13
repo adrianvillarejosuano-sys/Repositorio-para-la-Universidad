2d0b6042c0ba5fd7d406e35fbe6eb2408a1ca56f
Adri357 <adrianvillarejosuano@gmail.com>
2025-11-13 13:30:15 +0100

    correccion del ejercicio y adicion del ejecutable

--- ejerciciosTema3/elegirPersonaje.c
+++ ejerciciosTema3/elegirPersonaje.c
@@ -8,20 +8,20 @@ char personaje;
 printf("elige un personaje mago con (m) , guerrero con (g) o arquero con (a):\n");
 scanf(" %s", &personaje);
 
-if (personaje != "m" && personaje != "g" && personaje != "a")
+if (personaje != 'm' && personaje != 'g' && personaje != 'a')
     {
     printf("personaje no valido\n");
     }
 
-if (personaje == "m")
+if (personaje == 'm')
     {
         printf("seras un mago\n");
     }
-else if (personaje == "g")
+else if (personaje == 'g')
     {
         printf("seras un guerrero\n");
     }
-else if (personaje == "a")
+else if (personaje == 'a')
     {
         printf("seras un arquero\n");
     }
