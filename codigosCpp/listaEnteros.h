14c565333ec88ae72f7c677509d310a1424b7587
Adri357 <adrianvillarejosuano@gmail.com>
2026-04-19 18:32:56 +0200

    actualizo y mejoro lista de enetros

--- codigosCpp/listaEnteros.h
+++ codigosCpp/listaEnteros.h
@@ -7,26 +7,58 @@ class listaEnteros
 {
 
 public:
-    //Array de 100 elementos
-    int elementos[100];
 
-    //añadimos longitud , los elementos de la lista van de elementos[0] hasta elementos[longitud-1]
-    int longitud{0};
+    //constructor
+    listaEnteros();
+
+    //constructor que establece un numero inicial de elementos, todos ellos 0
+    listaEnteros(int longitud);
+
+    //destructor
+    ~listaEnteros();
+
+    //obtiene la longitud de la lista
+    int longitud;
+
+    //obtiene el elemento cuyo indice se indica
+    int getElemento(int indice);
+
+    //establece el valor del elemento indicado
+    void setElemento(int indice,int valor);
 
     //concatena un elemento al final de la lista
     void concatena(int valor);
-
-    //inserta un elemento en la lista en la posicion indicada por "indice"
+   
+    //inserta un elemento en la posicion indicada por indice
     void inserta(int indice , int valor);
 
-    //extrae el elemento de la lista en la posicion seleccionada
+    //extrae el elemento de la lista en la posicion indicada
     void extrae(int indice);
 
-    //metodos auxiliares
-    //desplazar los elementos a una posicion superior o inferior
-    void desplaza_arriba(int indice);
-    void desplaza_abajo(int indice);
 
+
+private:
+
+    //longitud del array "elementos"
+    int capacidad;
+
+    //array de capacidad enteros
+    //es un puntero que se usa como un array
+    int * elementos;
+
+    //longitud de la lista
+    //los elementos van desde [0]...[longtd-1]
+    int longtd;
+
+    //amplia cuando es necesario
+    void ampliar();
+
+    //desplaza arriba
+    void desplaza_arriba(int indice);
+
+    //desplaza abajo
+    void desplaza_abajo(int indice);
+    
 };
 
 
